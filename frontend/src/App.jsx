de240f287606c982efca51a0cc52cb777eb10a6f
yanezhvecnikar <i@euorik.ru>
2026-02-11 08:13:10 +0300

    Добавил Vite+React проект!

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>              
+      <h1 className="p-4 max-w-md mx-auto text-3xl font-bold">Vite + React</h1>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss(), react()],
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: ['backend', 'mysite.e'],
+  },
+})
